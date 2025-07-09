77c07556bb30ebc75edaf6a86806d1e4188490b6
Srinu-rj <dnsrinu143@gmail.com>
2025-07-09 17:50:47 +0530

    GitHub Actions:v1.0

---
+++ .github/jfrog-install.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+set -e
+
+echo "Authentication using $CRED";
+
+if [ $CRED = "username" ];
+then
+    sh -c "jfrog rt config --interactive=false --enc-password=true --url=$URL --user=$USER --password=$PASSWORD"
+elif [ $CRED = "apikey" ];
+then
+    sh -c "jfrog rt config --interactive=false --enc-password=true --url=$URL --apikey=$APIKEY"
+elif [ $CRED = "accesstoken" ];
+then
+    sh -c "jfrog rt config --interactive=false --enc-password=true --url=$URL --access-token=$ACCESSTOKEN"
+else
+    echo "ERROR! No authentication has been set :(";
+fi
+
+for cmd in "$@"; do
+    echo "Running '$cmd'..."
+    if sh -c "jfrog rt $cmd"; then
+        # no op
+        echo "Successfully ran '$cmd'"
+    else
+        exit_code=$?
+        echo "Failure running '$cmd', exited with $exit_code"
+        exit $exit_code
+    fi
+done
